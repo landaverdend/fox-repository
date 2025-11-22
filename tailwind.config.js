9e9e6fe110b48bcf084589615c8dab7fe74a855b
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-11-22 01:36:26 -0300

    more responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
         default: ['var(--font-inter)'],
       },
       colors: {
-        fox: '#dd8342', 
+        fox: '#dd8342',
         foxdark: '#b36734',
         foxbg: '#fff2d8',
         foxdarkbg: '#e6d6b3',
+        slate: '#3b2618',
       },
     },
   },
