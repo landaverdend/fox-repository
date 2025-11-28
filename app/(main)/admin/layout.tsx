97a3bdef35bec6aadb7e0c7dfc63792b71a82844
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-11-28 20:55:48 -0300

    admin layout.

---
+++ app/(main)/admin/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/navbar';
+import { stackServerApp } from '@/stack/server';
+import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
+
+export default async function AdminLayout({ children }: { children: React.ReactNode }) {
+  const user = await stackServerApp.getUser();
+
+  if (!user || !(await user.hasPermission('admin'))) {
+    redirect('/');
+  }
+
+  return (
+    <div className="bg-foxdarkbg min-h-screen">
+      <Suspense fallback={<div>Loading</div>}>{children}</Suspense>
+    </div>
+  );
+}
---
+++ app/(main)/admin/page.tsx
@@ -0,0 +1,6 @@
+import { stackServerApp } from "@/stack/server";
+
+export default async function AdminPage() {
+
+  return <div className="bg-foxdarkbg min-h-screen flex justify-center "></div>;
+}
