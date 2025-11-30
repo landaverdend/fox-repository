88dd877ea8b13992586d3466ebf1acd5a5702a9c
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-11-30 12:51:22 -0300

    emoji picker added

--- components/quote-card.tsx
+++ components/quote-card.tsx
@@ -2,6 +2,8 @@
 
 import { ParsedQuote, ParsedQuoteLine, parseQuote } from '@/lib/quoteParser';
 import { Quote } from '@/types/types';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
+import EmojiPicker from 'emoji-picker-react';
 
 type QCProps = {
   quote: Quote;
@@ -21,7 +23,25 @@ export default function QuoteCard({ quote, className }: QCProps) {
       {parsedQuote.lines.map((line) => (
         <QuoteCardContent key={line.text} line={line} />
       ))}
-      <div className="absolute bottom-[-12px] left-[5px] px-2 pb-[1px] bg-foxlight border border-foxdark text-white rounded-full text-sm">+</div>
+
+      <Popover>
+        <PopoverButton
+          className="absolute bottom-[-12px] left-[5px] px-2 pb-[1px] bg-foxlight/80 border border-foxdark text-white rounded-full text-sm select-none cursor-pointer hover:bg-foxlight/60"
+          onClick={(e) => {}}>
+          +
+        </PopoverButton>
+
+        <PopoverPanel anchor="bottom start" className="">
+          <EmojiPicker
+            onEmojiClick={(emoji) => console.log(emoji)}
+            className="!w-[290px] xs:!w-[350px]"
+            autoFocusSearch={false}
+            previewConfig={{
+              showPreview: false,
+            }}
+          />
+        </PopoverPanel>
+      </Popover>
     </div>
   );
 }
