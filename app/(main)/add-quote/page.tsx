43986c8a64d2d583c0ddd6aaa660bef2c8636a36
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-11-30 01:12:18 -0300

    fixed screen bug.

--- app/(main)/add-quote/page.tsx
+++ app/(main)/add-quote/page.tsx
@@ -25,7 +25,7 @@ export default function AddQuotePage() {
   };
 
   return (
-    <div className="bg-foxdarkbg min-h-screen flex flex-col items-center gap-20 text-slate md:flex-row md:gap-5 md:items-start md:justify-center">
+    <div className="bg-foxdarkbg min-h-screen flex flex-col items-center gap-20 text-slate md:flex-row md:items-start md:justify-center">
       <div className="mt-10 flex flex-col gap-4 items-center md:flex-row md:items-start">
         <div className="flex flex-col gap-2 justify-center items-center bg-foxbg p-2 rounded-md ">
           <h2 className="text-xl font-semibold">Quote Blocks</h2>
@@ -49,7 +49,7 @@ export default function AddQuotePage() {
           </AddButton>
         </div>
         {/* Stack component for draft quote lines */}
-        <div className="flex flex-col gap-2 bg-foxbg p-4 rounded-md items-center w-full min-h-[200px] min-w-[300px]">
+        <div className="flex flex-col gap-2 bg-foxbg p-4 rounded-md items-center w-full min-h-[200px] min-w-[255px]">
           <h2 className="text-xl font-semibold">Quote Editor</h2>
 
           {draftQuote.lines.map((line, i) => {
@@ -124,20 +124,19 @@ export default function AddQuotePage() {
             }
           })}
         </div>
-      </div>
+        <div className="flex flex-col items-center justify-center gap-2 bg-foxbg rounded-md p-4">
+          <h2 className="text-xl font-semibold">Quote Preview:</h2>
 
-      <div className="flex flex-col items-center justify-center gap-2 mt-10">
-        <h2 className="text-xl font-semibold">Quote Preview:</h2>
+          <div className="flex flex-col gap-2 min-w-[225px] lg:w-[300px] min-h-[100px] ">
+            {draftQuote.lines.map((line, index) => (
+              <QuoteCardContent key={line.text + index} line={line} />
+            ))}
+          </div>
 
-        <div className="flex flex-col gap-2 bg-foxbg p-4 rounded-md w-[300px] min-h-[100px] ">
-          {draftQuote.lines.map((line, index) => (
-            <QuoteCardContent key={line.text + index} line={line} />
-          ))}
+          <button className="bg-green-500 text-white text-semibold text-2xl px-4 py-2 rounded-md " onClick={saveQuote}>
+            Save Quote
+          </button>
         </div>
-
-        <button className="bg-green-500 text-white text-semibold text-2xl px-4 py-2 rounded-md " onClick={saveQuote}>
-          Save Quote
-        </button>
       </div>
     </div>
   );
@@ -174,7 +173,7 @@ function AddButton({ onClick, children }: AddButtonProps) {
   return (
     <button
       type="button"
-      className="bg-foxdark text-white px-2 py-2 rounded-md text-2xl cursor-pointer hover:opacity-80 h-fit w-full md:w-[200px]"
+      className="bg-foxdark text-white px-2 py-2 rounded-md text-2xl cursor-pointer hover:opacity-80 h-fit w-full md:w-[200px] md:text-xl"
       onClick={onClick}>
       {children}
     </button>
